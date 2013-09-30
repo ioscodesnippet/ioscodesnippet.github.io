51119884f301c0c623ba79b221513f57fbac0d27
Jamz Tang <jamz@jamztang.com>
2013-09-30 13:19:59 +0800

    Initial Commit

---
+++ _includes/ioscodesnippet/ioscodesnippet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ioscodesnippet
+//
+//  Created by Jamz Tang on 30/9/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ _includes/ioscodesnippet/ioscodesnippet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ioscodesnippet
+//
+//  Created by Jamz Tang on 30/9/13.
+//  Copyright (c) 2013 Jamz Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
